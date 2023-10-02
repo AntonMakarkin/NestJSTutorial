6c0845062a3ed6a327244d2a0ba8afc0d1c73701
AntonMakarkin <amakarkin27@gmail.com>
2023-10-02 23:24:22 +0300

    first commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Post, Get, UseGuards } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { User } from './users.model';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+
+@ApiTags('Пользователи')
+@Controller('users')
+export class UsersController {
+    
+    constructor(private usersService: UsersService) {}
+
+    @ApiOperation({ summary: 'Создание пользователя' })
+    @ApiResponse({ status: 200, type: User })
+    @Post()
+    create(@Body() userDto: CreateUserDto) {
+        return this.usersService.createUser(userDto);
+    }
+    
+    @ApiOperation({ summary: 'Получить всех пользователей' })
+    @ApiResponse({ status: 200, type: [User] })
+    @UseGuards(JwtAuthGuard)
+    @Get()
+    getAll() {
+        return this.usersService.getAllUsers();
+    }
+}
